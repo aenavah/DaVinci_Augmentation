2eed44b69403201f8635e07187e8a6a5921418bb
Alexandra Nava <aenava@ucsc.edu>
2024-01-28 17:07:46 -0800

    successfully creates output directory and patch, full, and seed subdirs in output

--- Davinci_Image_Augmentation.py
+++ Davinci_Image_Augmentation.py
@@ -1,16 +1,17 @@
 
 import cv2
 import os 
+import os.path 
 import numpy
 import pandas as pd
 from PIL import Image
+import shutil
 
 def subbest_dirs(root_dir): #might just read in count csv instead of doing this, when patch folders are already made it looks in those
   subs = []
   for root, dirs, files in os.walk(root_dir):
     if not dirs:
       subs.append(root)
-    print(subs) 
   return subs
 
 def create_output_folders(target_path, seed_list):
@@ -20,35 +21,34 @@ def create_output_folders(target_path, seed_list):
       os.makedirs(folder_path)
       for seed in seed_list:
         os.makedirs(folder_path + "/" + "s" + str(seed))
-    if os.path.exists(folder_path):
-      continue
-     
-  
+
+def copy_directories(src, dst):
+    os.makedirs(dst, exist_ok=True)
+    for item in os.listdir(src):
+        source_path = os.path.join(src, item)
+        destination_path = os.path.join(dst, item)
+        if os.path.isdir(source_path):
+            copy_directories(source_path, destination_path)
 
 if __name__ == "__main__":
   #begin user inputs
   exp_type = "Device"
-  base = "/Users/alexandranava/Desktop/DARPA/Tasks/DaVinci_Augmentation/"
-  # count_data_path = "/Users/alexandranava/Desktop/DARPA/Tasks/Count_Control_Images/Count Data/" + "Data_" + exp_type + "_V3.csv"
-  # count_data_pd = pd.read_csv(count_data_path)
-  # dropbox_paths = count_data_pd["Dropbox Path"].str.replace("Research and Data/Porcine Experiment at Davis/", "").to_list()
-  # print(dropbox_paths[0])
-
-
-  image_root = base + "Test Images"
-  types = ["Patch", "Full"]
+  image_folder = "/Users/alexandranava/Desktop/DARPA/Tasks/DaVinci_Augmentation/Test Images"
+  output_folder = "/Users/alexandranava/Desktop/DARPA/Tasks/DaVinci_Augmentation/Augmented Images"
   seed_list = [0, 1]
-  output_folders_exists = 0
 
-  #end user inputs
-  img_folders = subbest_dirs(image_root) #returns list of deepest folders in image_root
-    for folder in img_folders: #for each folder in the list of deepest_folders
-      create_output_folders(folder, seed_list)
-  else:
-    pass
+  print("Running...")
+  #Creates directory for output if it doesnt exists 
+  if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+    print("Created folder Augmented Images in " + output_folder)
+    copy_directories(image_folder, output_folder) #copy directory tree to output folder
+    image_folder = subbest_dirs(output_folder) #get lowest directories in output folder
+    for folder in image_folder:
+      create_output_folders(folder, seed_list) #make folders patch, full, and seeds inside 
 
+  print("Output folder Augmented Images already exists...")
 
-
-
+  
 
 
