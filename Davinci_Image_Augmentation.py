a2971b9571936b8e2ab4bb6fc8f23bf878c59bdd
Alexandra Nava <aenava@ucsc.edu>
2024-01-28 17:21:25 -0800

    writes txt file in seed folders indicating seed value

--- Davinci_Image_Augmentation.py
+++ Davinci_Image_Augmentation.py
@@ -7,21 +7,14 @@ import pandas as pd
 from PIL import Image
 import shutil
 
+
+#functions to initialize output directory
 def subbest_dirs(root_dir): #might just read in count csv instead of doing this, when patch folders are already made it looks in those
   subs = []
   for root, dirs, files in os.walk(root_dir):
     if not dirs:
       subs.append(root)
   return subs
-
-def create_output_folders(target_path, seed_list):
-  for output_type in ["Patch", "Full"]:
-    folder_path = target_path + "/" + output_type
-    if not os.path.exists(folder_path): #if patch or full folder doesnt
-      os.makedirs(folder_path)
-      for seed in seed_list:
-        os.makedirs(folder_path + "/" + "s" + str(seed))
-
 def copy_directories(src, dst):
     os.makedirs(dst, exist_ok=True)
     for item in os.listdir(src):
@@ -29,10 +22,23 @@ def copy_directories(src, dst):
         destination_path = os.path.join(dst, item)
         if os.path.isdir(source_path):
             copy_directories(source_path, destination_path)
+def create_output_folders(target_path, seed_list):
+  for output_type in ["Patch", "Full"]:
+    folder_path = target_path + "/" + output_type
+    if not os.path.exists(folder_path): #if patch or full folder doesnt
+      os.makedirs(folder_path)
+      for seed in seed_list:
+        os.makedirs(folder_path + "/" + "s" + str(seed))
+        with open(folder_path + "/" + "s" + str(seed) + "/" + "info.txt", "w") as file:
+          file.write("s" + str(seed) + " has value: " + str(seed))
+
+##
+        
+
+
+
 
 if __name__ == "__main__":
-  #begin user inputs
-  exp_type = "Device"
   image_folder = "/Users/alexandranava/Desktop/DARPA/Tasks/DaVinci_Augmentation/Test Images"
   output_folder = "/Users/alexandranava/Desktop/DARPA/Tasks/DaVinci_Augmentation/Augmented Images"
   seed_list = [0, 1]
@@ -46,8 +52,8 @@ if __name__ == "__main__":
     image_folder = subbest_dirs(output_folder) #get lowest directories in output folder
     for folder in image_folder:
       create_output_folders(folder, seed_list) #make folders patch, full, and seeds inside 
-
-  print("Output folder Augmented Images already exists...")
+  else:
+    print("Output folder Augmented Images already exists...")
 
   
 
